f07acc20eef94dff44872724156a290f5e5da7c3
Mo <moh19951@outlook.com>
2019-11-28 10:41:23 +0100

    Uitwerking van de exercise

---
+++ piramide.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Hoeveel stapels wil je hebben?";
+$a = readline(">");
+for ($i = 1; $i < 5; $i++) {
+    for ($j = $i; $j <= 5; $j++) {
+        echo "\n";
+    }
+    for ($j = 1; $j <= $i; $j++) {
+        echo " * ";
+    }
+    echo '';
+}
+
